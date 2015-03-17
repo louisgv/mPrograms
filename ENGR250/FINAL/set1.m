a0c2c8c978ebfd28f99a5854db92d8e25314cf6e
Louis Graham Vichy <lgvichy@gmail.com>
2015-03-16 21:40:06 -0700

    Just some stuff//

--- ENGR250/FINAL/set1.m
+++ ENGR250/FINAL/set1.m
@@ -1,7 +1,7 @@
 function set1()
 % Set 1 ----------------------------------------------
 % Linear Systems
-% Numerical solutions of Poisson’s equation
+% Numerical solutions of Poissonï¿½s equation
 
 % As discussed in class on 3/10-11, Solving the equation ultimately leaves 
 % us solving a linear system of the form Ax = b
@@ -10,11 +10,19 @@ function set1()
 
 % d2p(x)/d2x = r(x)
 
-% Solve this equation for x in [0,10] with , dx = 0.1
+% Solve this equation for x in [0,10] with
+
+dx = 0.1;
+x = 0:dx:10;
+
 % And subject to boundary condition:
-% r(x) = x in [0,2.5]
-% r(x) = 5-x in [2.5,7.5]
-% r(x) = -10+x in [7.5,10]
+r = @(x)( (x >= 0   && x <= 2.5).*x + ...
+          (x >= 2.5 && x <= 7.5).*(5-x) + ...
+          (x >= 7.5 && x <= 10 ).*(x-10) );
+
+x_b = dx:dx:10-dx;
+
+b = arrayfun(r,(x_b'))*(dx^2);
 
 % This type of boundary condition, where the function value is 
 % specified on the boundary, is called a Dirichlet boundary condition.
@@ -26,22 +34,18 @@ function set1()
 
 % Which is valid at each internal grid point. 
 % Building the Matrix A can be done in a fairly automated fashion 
-% with the “diag” function in MATLAB
+% with the ï¿½diagï¿½ function in MATLAB
 
-% Name your solution vector “Final1”, and plot its value vs 
-x = 0:0.1:10;
+A = diag(ones(1,98),1) + diag(-2*ones(1,99)) + diag(ones(1,98),-1);
 
-coeff = diag(-2*ones(1,99)) + diag(ones(1,98),1) + diag(ones(1,98),-1);
+p = A\b;
 
+% Name your solution vector
+Final1 = [0;p;0];
 
+% and plot its value vs
 
-dy = @ (t,y) -10.8*y;
-
-[t, y] = ode45 (dy, [0,3], 1);
-
-Final1 = y;
-
-plot (x, Final1,'-.k', t, exp(-10.8*t), '--r');
+plot (x, Final1,'-.k');
 
 end
 
--- ENGR250/FINAL/set2.m
+++ ENGR250/FINAL/set2.m
@@ -3,181 +3,212 @@ function set2()
 % Interpolation and Numerical Differentiation
 
 % The value of splines
-% In class we discussed the value of splines and how they
-% do more than make a graph look pretty.
+% In class we discussed the value of splines and how they 
+% do more than make a graph look pretty. 
 
-% The purpose of this problem is to provide an example of the value
+% The purpose of this problem is to provide an example of the value 
 % of splines by combining several things we have now learned.
 
-% We are going to examine y = sin(x) and
-% its derivatives based on coarse data.
+% We are going to examine y = sin(x) and 
+% its derivatives based on coarse data. 
 % Let
-xCoarse = 0:0.5:5;
+dxCoarse = 0.5;
+xCoarse = 0:dxCoarse:5;
 %  and
 yCoarse = sin(xCoarse);
-% Imagine, for the sake of this
-% problem, that these two vectors are data
+% Imagine, for the sake of this 
+% problem, that these two vectors are data 
 % that come from measurements,
-% and you are unaware that they come from
 
-% Your task is to calculate the first and
-% second derivatives in 5 different ways.
-% For many of these methods, we will need a
+% Your task is to calculate the first and 
+% second derivatives in 5 different ways. 
+% For many of these methods, we will need a 
 % finer x vector. Create a fine x vector,
-xFine = 0:0.01:5;
+dxFine = 0.01;
+xFine = 0:dxFine:5;
 % The five methods for the derivatives are:
 
 % 1) True Derivative
-% The analytical solution to the first and
-% second derivative, evaluated on xFine.
+% The analytical solution to the first and 
+% second derivative, evaluated on xFine. 
 
-% Call the first derivative
-dydxtrue = 0;
-% and second derivative
-d2ydx2true = 0;
+% Call the first derivative 
+dydxtrue = cos(xFine);
+% and second derivative 
+d2ydx2true = -sin(xFine);
 
 % 2) Coarse Finite difference
-% Use O(del(x)^2) accurate finite difference
-% schemes on the coarse data.
+% Use O(del(x)^2) accurate finite difference 
+% schemes on the coarse data. 
 
-% Call the derivatives
-dydxcoarse = 0;
-% and
-d2ydx2coarse = 0;
+n = length(yCoarse);
 
-% 3) Finite difference on �nearest� interpolation
+% Call the derivatives 
+dydxcoarse = zeros(n);
+    dydxcoarse(1)= (-3*yCoarse(1)+4*yCoarse(2)-yCoarse(3))/(2*dxFine);
+        for i=2:n-1
+            dydxcoarse(i) = (yCoarse(i+1)-yCoarse(i-1))/(2*dxFine); 
+        end
+    dydxcoarse(n)=(3*yCoarse(n)-4*yCoarse(n-1) + yCoarse(n-2))/(2*dxFine);
+% and 
+d2ydx2coarse = zeros(n);
+    d2ydx2coarse(1)=  (2*yCoarse(1) - 5*yCoarse(2) + 4*yCoarse(3)-yCoarse(4))/dxFine^2;
+        for i=2:n-1
+            d2ydx2coarse(i) = (yCoarse(i+1)-2*yCoarse(i) + yCoarse(i-1))/dxFine^2;
+        end
+    d2ydx2coarse(n)=  (2*yCoarse(n) - 5*yCoarse(n-1) + 4*yCoarse(n-2)-yCoarse(n-3))/dxFine^2;
+
+
+% 3) Finite difference on "nearest" interpolation
 % Using O(del(x)^2) accurate finite difference
-% schemes on the coarse data interpolated
-% on xFine. Use the interp1 function with
-% the �nearest� option.
+% schemes on the coarse data interpolated 
+% on xFine. Use the interp1 function with 
+% the nearest option. 
 
-% Call the derivatives
-dydxnearest = 0;
-% and
-d2ydx2nearest= 0;
+% Call the derivatives 
+dydxnearest = interp1(xCoarse, dydxcoarse, xFine, 'nearest');
+% and 
+d2ydx2nearest= interp1(xCoarse, d2ydx2coarse, xFine, 'nearest');
 
 % 4) Finite difference on linear interpolation
-% Using O(del(x)^2) accurate finite difference
-% schemes on the coarse data interpolated
-% on xFine. Use the interp1 function with
-% the �linear� option.
+% Using O(del(x)^2) accurate finite difference % schemes on the coarse data interpolated 
+% on xFine. Use the interp1 function with 
+% the "linear" option. 
 
-% Call the derivatives
-dydxlinear = 0;
-% and
-d2ydx2linear = 0;
+% Call the derivatives 
+dydxlinear = interp1(xCoarse, dydxcoarse, xFine);
+% and 
+d2ydx2linear = interp1(xCoarse, d2ydx2coarse, xFine);
+
+%5) Finite difference on spline interpolation
+% Using O(del(x)^2) accurate finite difference 
+% schemes on the coarse data interpolated 
+% on xFine. Use the interp1 function with the 
+% 'spline' option. 
+
+% Call the derivatives 
+dydxspline = interp1(xCoarse, dydxcoarse, xFine, 'spline');
+%and 
+d2ydx2spline = interp1(xCoarse, d2ydx2coarse, xFine, 'spline');
 
 % Create three plots:
-% one for the function itself,
-% For the plot of the function,
-% plot all 5 methods.
+% For the plot of the function, 
+% plot all 5 methods. 
+figure; hold on;
+plot(xCoarse, yCoarse);
+hold off;
 
-% one for the first derivative, and
-% For the derivative, plot all except �nearest�
+% For the derivative, plot all except "nearest" 
 
-% one for the second derivative.
-% For the second derivative, plot all except
-% the linear and spline interpolation.
+figure; hold on; 
+plot(xCoarse,dydxcoarse,xFine,dydxlinear,xFine,dydxspline);
+hold off;
 
+% For the second derivative, plot all except 
+% the linear and spline interpolation. 
 
-% Notice how the accuracy of the
-% interpolation methods degrades as
-% higher derivatives are taken.
+figure; hold on; 
+plot(xCoarse,d2ydx2coarse,xCoarse,d2ydx2nearest);
+hold off;
 
-% For each plot, make sure when you plot
-% the coarse data you use a marker (like �o�)
-% instead of a line, since this is only
+% Notice how the accuracy of the 
+% interpolation methods degrades as 
+% higher derivatives are taken. 
+
+% For each plot, make sure when you plot 
+% the coarse data you use a marker (like "o") 
+% instead of a line, since this is only 
 % known at coarse locations.
 
 
 
 %-------------Notes--------------------------------------------------------
-% To make the plots and lines easier to
-% distinguish, I recommend using the
-% �legend� and �title� commands in Matlab.
+% To make the plots and lines easier to 
+% distinguish, I recommend using the 
+% "legend" and "title" commands in Matlab.
 
-% When option 1) above is unavailable
-% (i.e., the function is not known),
-%     and a derivative is needed,
+% When option 1) above is unavailable 
+% (i.e., the function is not known), 
+%     and a derivative is needed, 
 
-%     (2) is the most obvious option,
-%     but gives the derivative based only
-%     on the coarse data.
+%     (2) is the most obvious option, 
+%     but gives the derivative based only 
+%     on the coarse data. 
 
-%     With interpolation, we can get
-%     finer data, but the accuracy degrades
+%     With interpolation, we can get 
+%     finer data, but the accuracy degrades 
 %     quickly when we take higher derivatives.
-%     This is an advantage of splines.
+%     This is an advantage of splines. 
 
-%     With higher order polynomial
-%     interpolation, we can get even better
-%     results that remain accurate to
+%     With higher order polynomial 
+%     interpolation, we can get even better 
+%     results that remain accurate to 
 %     higher derivatives.
 %-------------EoN----------------------------------------------------------
 
-% Answer the following questions in comments
+% Answer the following questions in comments 
 % in your m-file:
 
-% 1) Regarding the plot of the function,
-% rank the three interpolation methods
+% 1) Regarding the plot of the function, 
+% rank the three interpolation methods 
 % from best to worst at:
 % a) Fitting the data (x_coarse, y_coarse)
 %-------------Respond Below--------------------
-%
-%
-%
+% 
+% 
+% 
 %-------------End of Respond-------------------
 % b) Fitting the parent function, (y = sin(x))
 %-------------Respond Below--------------------
-%
-%
-%
+% 
+% 
+% 
 %-------------End of Respond-------------------
-% 2) Regarding the calculation of the
-% first derivative, rank the 4 methods
-% from best to worst at fitting the
-% parent function derivative, dy/dx = cos(x).
+% 2) Regarding the calculation of the 
+% first derivative, rank the 4 methods 
+% from best to worst at fitting the 
+% parent function derivative, dy/dx = cos(x). 
 %-------------Respond Below--------------------
-%
-%
-%
+% 
+% 
+% 
 %-------------End of Respond-------------------
-% How would you rate the accuracy
-% of the spline considering
+% How would you rate the accuracy 
+% of the spline considering 
 % how coarse the data was it came from?
 %-------------Respond Below--------------------
-%
-%
-%
+% 
+% 
+% 
 %-------------End of Respond-------------------
-% 3) Regarding the calculation of the
-% second derivative, rank the 4 methods
-% from best to worst at fitting the
-% parent function
+% 3) Regarding the calculation of the 
+% second derivative, rank the 4 methods 
+% from best to worst at fitting the 
+% parent function 
 % second derivative, d2y/d2x =-sin(x)
 %-------------Respond Below--------------------
-%
-%
-%
-%
+% 
+% 
+% 
+% 
 %-------------End of Respond-------------------
 
 %-------------Notes--------------------------------------------------------
-% While the results here are specific
-%   to this problem, they generalize well
-%   so long as our data comes from a function
-%       which is infinitely differentiable
-%       (we can take its derivative as
-%       many times as we like),
-%       which is how most physical
+% While the results here are specific 
+%   to this problem, they generalize well 
+%   so long as our data comes from a function 
+%       which is infinitely differentiable 
+%       (we can take its derivative as 
+%       many times as we like), 
+%       which is how most physical 
 %       functions behave.
 %-------------EoN----------------------------------------------------------
 
-% Hope you had a good time in the class,
+% Hope you had a good time in the class, 
 %-------------Respond Below--------------------
 % Sure did! Thanks for the knowledge, Sir!
 %-------------End of Respond-------------------
-%   and good luck in future endeavors.
+%   and good luck in future endeavors. 
 %-------------Respond Below--------------------
 % Thank you Sir!
 %-------------End of Respond-------------------
